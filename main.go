903130cb82a6cb480cb93e90c643aede03c11209
Valeriy Kostikov <mkoctuk@gmail.com>
2018-03-05 11:50:31 +0300

    add panic

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"log"
+
 	"github.com/serbe/adb"
 )
 
 func main() {
 	cfg, err := getConfig()
 	if err != nil {
-		return
+		log.Panic(err)
 	}
 	db = adb.InitDB(
 		cfg.Base.Name,
